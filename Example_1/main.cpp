e10079f91b699052bdf23681eacdbfeb64d149ee
ladev325 <vlad2008artemov@gmail.com>
2025-04-29 18:44:02 +0300

    a

--- Example_1/main.cpp
+++ Example_1/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "Soft.h"
-
+#include "Date.h"
 using namespace std;
 
 int main() {
-	Commercial commercial;
+	Commercial commercial("name", "producer", Date(1, 1, 2001), 10, 1000);
 	commercial.showInfo();
 
 	return 0;
