87241a14e865cf06880e2d982a810486aa6f06bd
ladev325 <vlad2008artemov@gmail.com>
2025-05-06 16:39:53 +0300

    e

--- Example_1/main.cpp
+++ Example_1/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "Soft.h"
-#include "Date.h"
 using namespace std;
 
 int main() {
-	Commercial commercial("name", "producer", Date(1, 1, 2001), 10, 1000);
-	commercial.showInfo();
 
 	return 0;
 }
--- main.cpp
+++
@@ -1,2 +0,0 @@
-﻿#include <iostream>
-using namespace std;
